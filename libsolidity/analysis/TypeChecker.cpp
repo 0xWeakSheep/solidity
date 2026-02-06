a46aa13d00c9b4ed5207a8ec463aba43ce45188b
rodiazet <radek.zagorowicz@gmail.com>
2026-02-06 15:35:35 +0100

    Add doc strings and improve naming.

--- libsolidity/analysis/TypeChecker.cpp
+++ libsolidity/analysis/TypeChecker.cpp
@@ -3203,7 +3203,7 @@ void TypeChecker::validateAccessMemberFunctionType(
 	Type const* _expressionObjectType,
 	ASTString const& _memberName,
 	SourceLocation const& _location,
-	bool _emptyArguments,
+	bool _hasArguments,
 	bool _isDefined
 ) const
 {
@@ -3228,7 +3228,7 @@ void TypeChecker::validateAccessMemberFunctionType(
 
 	if (
 		_accessedMemberFunctionType->kind() == FunctionType::Kind::ArrayPush &&
-		!_emptyArguments &&
+		_hasArguments &&
 		_expressionObjectType->containsNestedMapping()
 	)
 		m_errorReporter.typeError(
@@ -3307,7 +3307,7 @@ bool TypeChecker::visit(MemberAccess const& _memberAccess)
 			expressionObjectType,
 			memberName,
 			_memberAccess.location(),
-			(*arguments).numArguments() == 0,
+			arguments && (*arguments).numArguments() > 0,
 			accessedMemberAnnotation.referencedDeclaration != nullptr
 		);
 	}
