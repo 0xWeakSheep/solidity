9386d8726b47146de7c809d91909aac101d4badf
Matheus Aguiar <matheus.pit@gmail.com>
2026-02-24 03:55:15 -0300

    Add tests

---
+++ test/libsolidity/semanticTests/errors/require_custom_error_constructor_side_effect.sol
@@ -0,0 +1,13 @@
+contract C {
+    error MyError(uint errorCode, string errorMsg);
+    uint public counter = 0;
+    function count() public returns (uint) { return ++counter; }
+    function f() external {
+        string memory eMsg = "error";
+        require(false, MyError(count(), eMsg));
+        counter = 42;
+    }
+}
+// ----
+// f() -> FAILURE, hex"0b9344e2", hex"0000000000000000000000000000000000000000000000000000000000000001", hex"0000000000000000000000000000000000000000000000000000000000000040", hex"0000000000000000000000000000000000000000000000000000000000000005", hex"6572726f72000000000000000000000000000000000000000000000000000000"
+// counter() -> 0
