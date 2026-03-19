5efedf84a687001b4c637b3364fe9fd4fbd93b86
Matheus Aguiar <matheus.pit@gmail.com>
2026-03-18 22:59:44 -0300

    fixup! Add and update tests

--- test/libsolidity/semanticTests/errors/require_custom_error_constructor_side_effect_revert_strings_strip.sol
+++ test/libsolidity/semanticTests/errors/require_custom_error_constructor_side_effect_revert_strings_strip.sol
@@ -1,17 +1,17 @@
 // The purpose of this test is to check that the side-effect of
-// the error parameter occurs and state is properly reverted
+// the error parameter occurs, i.e, MyError is constructed with errorCode = 1
+// and also confirm the state is properly reverted
 contract C {
-    error MyError(uint errorCode, string errorMsg);
+    error MyError(uint errorCode);
     uint public counter = 0;
     function count() public returns (uint) { return ++counter; }
     function f() external {
-        string memory eMsg = "error";
-        require(false, MyError(count(), eMsg));
+        require(false, MyError(count()));
         counter = 42;
     }
 }
 // ====
 // revertStrings: strip
 // ----
-// f() -> FAILURE, hex"0b9344e2", hex"0000000000000000000000000000000000000000000000000000000000000001", hex"0000000000000000000000000000000000000000000000000000000000000040", hex"0000000000000000000000000000000000000000000000000000000000000005", hex"6572726f72000000000000000000000000000000000000000000000000000000"
+// f() -> FAILURE, hex"30b1b565", hex"0000000000000000000000000000000000000000000000000000000000000001"
 // counter() -> 0
