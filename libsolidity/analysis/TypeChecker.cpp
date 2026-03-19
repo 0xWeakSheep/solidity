6627e28d9d898c2ef2b4fe6de059e76ac5ca3e4a
rodiazet <radek.zagorowicz@gmail.com>
2026-03-19 17:30:32 +0100

    Member access via a contract type name.
    - Foreign function declaration access via contract name are pure.
    - Functions declaration from derived scope are pure. Can be used to initialise constant function pointer.
    - Internal library function declaration are pure. Can be used to initialise constant function pointer.
    - struct, enum, UDVT declarations are pure.
    - Constant variable declaration in contract and library are pure.
    - event declaration is pure

--- libsolidity/analysis/TypeChecker.cpp
+++ libsolidity/analysis/TypeChecker.cpp
@@ -3418,13 +3418,75 @@ bool TypeChecker::visit(MemberAccess const& _memberAccess)
 		}
 		case Type::Category::Contract:
 		{
+			// Member of a contract accessed by the contract name (not contract instance).
+
+			// ContractType has only user-defined members, so accessedMemberAnnotation.referencedDeclaration is not `NULL`.
+			// See `ContractType::nativeMembers` for details.
+			solAssert(accessedMemberAnnotation.referencedDeclaration);
 			isAccessedMemberLValue = accessedMemberAnnotation.referencedDeclaration->isLValue();
-			if (
-				auto const* accessedMemberFunctionType = dynamic_cast<FunctionType const*>(accessedMemberAnnotation.type);
-				accessedMemberFunctionType &&
-				accessedMemberFunctionType->kind() == FunctionType::Kind::Declaration
-			)
-				accessedMemberAnnotation.isPure = *_memberAccess.expression().annotation().isPure;
+			// Expressions like `C.foo;`, `C.Ev;` are pure and they must generate `Statement has no effect.` warning.
+			// TODO: However, in case a function this does not allow to assign the expression to a constant variable,
+			// TODO: because of different kind. Left-hand side of the variable declaration never has `Declaration` kind.
+			if (auto const* functionTypeMember = dynamic_cast<FunctionType const*>(accessedMemberAnnotation.type))
+			{
+				// By default, all pure function invocation kinds are pure. Additionally, `C.Ev` is pure too.
+				// Note: This means also that a member function of a foreign contract accessed via the contract type
+				// name is pure, but a member function of a library accessed via the library name is not pure, because
+				// `C.foo` cannot be called (it needs a contract instance instead), but `Lib.foo` can be called.
+				if (
+					functionTypeMember->isPure() ||
+					functionTypeMember->kind() == FunctionType::Kind::Event
+				)
+					accessedMemberAnnotation.isPure = true;
+				else if (functionTypeMember->kind() == FunctionType::Kind::Internal)
+				{
+					// A variable declaration of constant function pointer.
+					if (
+						auto const* variableDeclarationMember =
+							dynamic_cast<VariableDeclaration const*>(accessedMemberAnnotation.referencedDeclaration)
+					)
+						accessedMemberAnnotation.isPure = variableDeclarationMember->isConstant();
+					else if (dynamic_cast<FunctionDefinition const*>(accessedMemberAnnotation.referencedDeclaration))
+						accessedMemberAnnotation.isPure = true;
+					else
+						solAssert(false, "Impossible declaration type for internal function call kind");
+				}
+				else if (functionTypeMember->kind() == FunctionType::Kind::External)
+				{
+					// A variable declaration of constant function pointer.
+					if (
+						auto const* variableDeclarationMember =
+							dynamic_cast<VariableDeclaration const*>(accessedMemberAnnotation.referencedDeclaration)
+					)
+						accessedMemberAnnotation.isPure = variableDeclarationMember->isConstant();
+					else
+						solAssert(
+							dynamic_cast<FunctionDefinition const*>(accessedMemberAnnotation.referencedDeclaration),
+							"Impossible declaration type for external function call kind"
+						);
+				}
+				else
+					// Library function declaration is not pure. It requires the library address.
+					solAssert(functionTypeMember->kind() == FunctionType::Kind::DelegateCall);
+			}
+			else if (auto const* typeTypeMember = dynamic_cast<TypeType const*>(accessedMemberAnnotation.type))
+			{
+				solAssert(
+					typeTypeMember->actualType()->category() == Type::Category::Struct ||
+					typeTypeMember->actualType()->category() == Type::Category::Enum ||
+					// Note: We add Contract intentionally, to cover a possible contract nesting case.
+					typeTypeMember->actualType()->category() == Type::Category::Contract ||
+					typeTypeMember->actualType()->category() == Type::Category::UserDefinedValueType,
+					"Impossible `TypeType` category as contract member."
+				);
+				accessedMemberAnnotation.isPure = true;
+			}
+			// In case `Base.value` or `Lib.value` and when `value` is constant, the expression is pure.
+			else if (auto const* varDecl = dynamic_cast<VariableDeclaration const*>(accessedMemberAnnotation.referencedDeclaration))
+				accessedMemberAnnotation.isPure = varDecl->isConstant();
+			else
+				solAssert(false, "Unexpected annotation type");
+
 			break;
 		}
 		case Type::Category::Enum:
@@ -3589,7 +3651,6 @@ bool TypeChecker::visit(MemberAccess const& _memberAccess)
 	case Type::Category::FixedBytes:
 	case Type::Category::Array:
 	case Type::Category::ArraySlice:
-	case Type::Category::Contract:
 	case Type::Category::Enum:
 	case Type::Category::UserDefinedValueType:
 	case Type::Category::Tuple:
@@ -3601,15 +3662,10 @@ bool TypeChecker::visit(MemberAccess const& _memberAccess)
 
 	accessedMemberAnnotation.isLValue = isAccessedMemberLValue;
 
-	// TODO: Leave it for now, but it should be moved to TypeType -> Contract case.
-	// We do not want to change the logic in refactor PR.
-	if (
-		auto const* varDecl = dynamic_cast<VariableDeclaration const*>(accessedMemberAnnotation.referencedDeclaration);
-		!accessedMemberAnnotation.isPure.set() &&
-		varDecl &&
-		varDecl->isConstant()
-	)
-		accessedMemberAnnotation.isPure = true;
+	// // TODO: Leave it for now, but it should be moved to TypeType -> Contract case.
+	// // We do not want to change the logic in refactor PR.
+	// if (dynamic_cast<VariableDeclaration const*>(accessedMemberAnnotation.referencedDeclaration))
+	// 	solAssert(accessedMemberAnnotation.isPure.set());
 
 	if (!accessedMemberAnnotation.isPure.set())
 		accessedMemberAnnotation.isPure = false;
